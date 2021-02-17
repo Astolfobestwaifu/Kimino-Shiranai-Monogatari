7daf2351e312e320dcd062ce159077a096887c7f
Astolfobestwaifu <79107750+Astolfobestwaifu@users.noreply.github.com>
2021-02-17 14:13:50 -0300

    Add files via upload

---
+++ あれがデネブ、アルタイル、ベガ.py
@@ -0,0 +1,9 @@
+AS = int ( input ('Quantos anos de estudo você tem, idiota?'))
+if 1 == AS or AS == 0 :
+    print("Iniciante... hehe")
+elif 3 >= AS and AS > 1 :
+    print("Intermediário")
+elif 6 >= AS and AS > 3:
+    print("Avançado")
+elif AS >= 7:
+    print("Jedi Master... Subarashii")
